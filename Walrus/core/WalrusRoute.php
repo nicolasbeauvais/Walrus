47673196c60164c730585bee938b86739ff4dcac
E-Wok <n.beauvais78@gmail.com>
2014-01-11 15:24:58 +0100

    Routing: basic routing upgraded, ready to yaml

--- Walrus/core/WalrusRoute.php
+++ Walrus/core/WalrusRoute.php
@@ -1,17 +1,21 @@
 <?php
 /**
  * Author: Walrus Team
- * "Created: 16:10 13/12/13
+ * Created: 16:10 13/12/13
  */
 
 namespace Walrus\core\Route;
 
+/**
+ * Class WalrusRoute
+ * @package Walrus\core\Route
+ */
 class WalrusRoute
 {
+
     private static $route;
     private static $route_with_params;
     private static $params;
-
     private static $instance;
 
     private function __construct()
@@ -19,7 +23,11 @@ class WalrusRoute
 
     }
 
-    public static function singleton()
+    /**
+     * Basic Singleton.
+     * @return mixed
+     */
+    public static function singleton ()
     {
         if (!isset(self::$instance)) {
             $obj = __CLASS__;
@@ -30,26 +38,33 @@ class WalrusRoute
         return self::$instance;
     }
 
+
+    /**
+     * Main routing methode, called in the kernel.
+     * Parse routing.yml to check the route and sanitize parameters
+     */
     private static function route ()
     {
+        //@TODO: yaml stuff
+        //walrus.com/controller/method/arg/param1/value1/param2/value2/
 
-        //walrus.com/controller/method/param1/value1/param2/value2/
         if (!isset($_GET['url'])) {
             self::$route = '';
             self::$route_with_params = '';
-            self::$params = '';
+            self::$params = array();
             return;
         }
 
         $route = explode('/', rtrim($_GET['url'], "/"));
         $count = count($route);
 
+        $controller = $route[0];
+
+        $params = array();
         if ($count < 2) {
-            trigger_error('Unavailable url', E_USER_ERROR);
             return;
         }
 
-        $controller = $route[0];
         $method = $route[1];
 
         if ($count > 2) {
@@ -64,23 +79,39 @@ class WalrusRoute
         }
 
         var_dump($controller, $method, isset($params) ? $params : 'no params');
+
     }
 
+    /**
+     * Return the current route as a string (without GET parameters).
+     * @return string
+     */
     public function getRoute()
     {
         return self::$route;
     }
 
+    /**
+     * Return the currents params as an array
+     * @return array
+     */
     public function getParams()
     {
-        return isset(self::$params) && !empty(self::$params) ? self::$params : false;
+        return self::$params;
     }
 
+    /**
+     * Return the current route with GET parameters.
+     * @return string
+     */
     public function getRouteWithParams()
     {
         return isset(self::$route_with_params) && !empty(self::$route_with_params) ? self::$route_with_params : false;
     }
 
+    /**
+     * Prevent cloning.
+     */
     public function __clone()
     {
         trigger_error('Cloning the Walrus Route class is not permitted', E_USER_ERROR);
