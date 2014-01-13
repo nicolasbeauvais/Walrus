fafa1bae49f1e3b2634821d010ca48736384ce47
FaustineLarmagna <f.larmagna@gmail.com>
2014-01-13 10:49:50 +0100

    Started bootstrap method in walruskernel

--- Walrus/core/WalrusKernel.php
+++ Walrus/core/WalrusKernel.php
@@ -29,7 +29,17 @@ class WalrusKernel
 
     private static function bootstrap()
     {
-
+      $config_file = "../../config/config.yml";
+      if (exists($config_file))
+      {
+	$array_info = yaml_parse_file($config_file);
+      }
+      else
+      {
+	$content = "";
+	file_put_contents($config_file, $content, FILE_APPEND);
+      }
+	
         //configuration here
     }
 }
