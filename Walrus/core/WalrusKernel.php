9b73bcc35959df4c285468c6d0f6ca3a05e4e98f
FaustineLarmagna <f.larmagna@gmail.com>
2014-01-13 11:34:42 +0100

    Continue default config

--- Walrus/core/WalrusKernel.php
+++ Walrus/core/WalrusKernel.php
@@ -23,11 +23,35 @@ class WalrusKernel
       $config_file = "../../config/config.yml";
       if (exists($config_file))
       {
-	$array_info = yaml_parse_file($config_file);
+	$array_info = \Spyc::YAMLLoad($config_file);
+	// \Spyc::YAMLDump($array, 4, 60)
+
+	if ($array_info->templating->default == 'haml')
+	  //installer haml
+	elseif ($array_info->templating->default == 'smarty')
+	  //installer smarty
+	else
+	  //installer twig
+
+	if ($array_info->database->language == 'MySQL')
+	{
+	  //installer MySQL
+	  $dbname = $array_info->database->name;
+	  $dbpwd = $array_info->database->password;
+	}
+
       }
       else
       {
-	$content = "";
+	$content = '
+database:
+    language:  "MySQL"
+    name: "project"
+    password: ""
+
+templating:
+    default: "haml"
+';
 	file_put_contents($config_file, $content, FILE_APPEND);
       }
 	
