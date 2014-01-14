70ce8e2f0b450c615dd4697aee382ef23dbd440e
FaustineLarmagna <f.larmagna@gmail.com>
2014-01-14 11:21:39 +0100

    Fixed first errors on basic config

--- Walrus/core/WalrusKernel.php
+++ Walrus/core/WalrusKernel.php
@@ -20,42 +20,51 @@ class WalrusKernel
 
     private static function bootstrap()
     {
-      $config_file = "../../config/config.yml";
-      if (exists($config_file))
-      {
-	$array_info = \Spyc::YAMLLoad($config_file);
-	// \Spyc::YAMLDump($array, 4, 60)
-
-	if ($array_info->templating->default == 'haml')
-	  //installer haml
-	elseif ($array_info->templating->default == 'smarty')
-	  //installer smarty
-	else
-	  //installer twig
-
-	if ($array_info->database->language == 'MySQL')
+      $config_file = "../config/config.yml";
+      if (file_exists($config_file))
 	{
-	  //installer MySQL
-	  $dbname = $array_info->database->name;
-	  $dbpwd = $array_info->database->password;
+	  $array_info = \Spyc::YAMLLoad($config_file);
+	  // \Spyc::YAMLDump($array, 4, 60)
+	  
+	  if ($array_info['templating']['default'] == 'haml')
+	    {
+	      //installer haml
+	    }
+	  elseif ($array_info['templating']['default'] == 'smarty')
+	    {
+	      //installer smarty
+	    }
+	  else
+	    {
+	      //installer twig
+	    }
+	  
+	  if ($array_info['database']['language'] == 'MySQL')
+	    {
+	      //installer MySQL
+	      $dbname = $array_info['database']['name'];
+	      $dbpwd = $array_info['database']['password'];
+	    }
+	  else
+	    {
+	      // autre db ?
+	    }
 	}
-
-      }
       else
-      {
-	$content = '
-database:
-    language:  "MySQL"
-    name: "project"
-    password: ""
-
-templating:
-    default: "haml"
-';
-	file_put_contents($config_file, $content, FILE_APPEND);
-      }
-	
-        //configuration here
+	{
+	  $php_content = array(
+			       "database" => array("language" => "MySQL",
+						   "name" => "project",
+						   "password" => ""),
+			       "templating" => array("default" => "haml")
+			       );
+	  
+	  $yml_content = \Spyc::YAMLDump($content_php);
+	  
+	  file_put_contents($config_file, $yml_content, FILE_APPEND);
+	}
+      
+      //configuration here
     }
 }
 
