f455ddeb2289abb801069c7735f75a6ee83dc247
Nicolas Beauvais <n.beauvais78@gmail.com>
2014-01-16 15:32:17 +0100

    Index: rework entire index.php file and move autoload to a specific Walrus class

--- www/index.php
+++ www/index.php
@@ -13,36 +13,8 @@ use Walrus\core\WalrusKernel as WalrusKernel;
 define("APP_PATH", dirname(__FILE__) . '/');
 define("ROOT_PATH", substr(dirname(__FILE__), 0, -4) . '/');
 
-function __autoload ($called_class)
-{
-    if (strrpos($called_class, "\\")) {
-        $exploded_class = explode('\\', $called_class);
-        $class_name = array_pop($exploded_class);
-    } else {
-        $class_name = $called_class;
-    }
+require_once('../Walrus/core/WalrusAutoload.php');
 
-    if (is_file(ROOT_PATH . 'Walrus/controllers/' . $class_name . '.php')) {
-        require_once(ROOT_PATH . 'Walrus/controllers/' . $class_name . '.php');
-
-    } elseif (is_file(ROOT_PATH . 'Walrus/core/' . $class_name . '.php')) {
-        require_once(ROOT_PATH . 'Walrus/core/' . $class_name . '.php');
-
-    } elseif (is_file(ROOT_PATH . 'Walrus/core/route/' . $class_name . '.php')) {
-        require_once(ROOT_PATH . 'Walrus/core/route/' . $class_name . '.php');
-
-    } elseif (is_file(ROOT_PATH . 'Walrus/models/' . $class_name . '.php')) {
-        require_once(ROOT_PATH . 'Walrus/models/' . $class_name . '.php');
-
-    } elseif (is_file(ROOT_PATH . 'vendors/' . $class_name . '.php')) {
-        require_once(ROOT_PATH . 'vendors/' . $class_name . '.php');
-
-    } elseif (is_file(ROOT_PATH . 'engine/controllers/' . $class_name . '.php')) {
-        require_once(ROOT_PATH . 'engine/controllers/' . $class_name . '.php');
-
-    } else {
-        return false;
-    }
-}
+new \Walrus\core\WalrusAutoload();
 
 WalrusKernel::execute();
