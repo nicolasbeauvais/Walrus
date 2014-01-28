2aebc2f1b47343384d2740faf7b6a203062a1cde
Nicolas Beauvais <n.beauvais78@gmail.com>
2014-01-28 23:17:12 +0100

    WalrusCLI: 'php tusk createController foo' work ;)

--- Walrus/core/WalrusCLI.php
+++ Walrus/core/WalrusCLI.php
@@ -5,10 +5,10 @@
  * File maintened by: Nicolas Beauvais (E-Wok)
  * Created: 13:26 28/01/14
  */
- 
 
 namespace Walrus\core;
 
+use Exception;
 
 class WalrusCLI
 {
@@ -47,6 +47,50 @@ class WalrusCLI
 
     private static function createController ($name)
     {
+        if (strpbrk($name, "\\/?%*:|\"<>")) {
+            echo $name . ' isn\'t a valid controller name' . "\n";
+            return;
+        }
+
+        $name = ucwords(strtolower($name));
+        $filer = new WalrusFileManager(ROOT_PATH);
+
+        if (!file_exists(ROOT_PATH . 'engine/controllers/' . $name . 'Controller.php')) {
+
+            // @TODO: catch isn't working ?
+            try {
+                $filer->setCurrentElem('Walrus/core/sample/controller.sample');
+                $controller = $filer->getFileContent();
+                $controller = str_replace('%name%', $name, $controller);
+
+                $filer->setCurrentElem('engine/controllers');
+                $filer->fileCreate($name . 'Controller.php');
+                $filer->setCurrentElem('engine/controllers/' . $name . 'Controller.php');
+                $filer->changeFileContent($controller);
+                echo 'New controller created in engine/controllers with the name ' . $name . 'Controller.php' . "\n";
+            } catch (Exception $e) {
+                echo 'Exception: ' . $e->getMessage() . "\n";
+                return;
+            }
+        } else {
+            echo $name . 'Controller.php already exist' . "\n";
+        }
+
+        if (!file_exists(ROOT_PATH . 'www/templates/' . $name)) {
+
+            try {
+
+                $filer->setCurrentElem('www/templates');
+                $filer->folderCreate(strtolower($name));
+
+                echo 'New templates directory created in www/templates with the name ' . $name . "\n";
+            } catch (Exception $e) {
+                echo 'Exception: ' . $e->getMessage() . "\n";
+                return;
+            }
+        } else {
+            echo $name . ' template directory already exist' . "\n";
+        }
 
     }
 }
