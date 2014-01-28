6377625e9ec198cd75049c70067357d6fcf6c450
Nicolas Beauvais <nicolas@bvs.email>
2014-01-28 11:10:59 +0100

    WalrusFrontController: isntance template possibility + protected variables

--- Walrus/core/WalrusFrontController.php
+++ Walrus/core/WalrusFrontController.php
@@ -8,23 +8,54 @@
 
 namespace Walrus\core;
 
+use Walrus\core\entity\Template as Template;
 use MtHaml;
+use Exception;
 
 class WalrusFrontController
 {
-    private $template = '';
+    /**
+     * All requested templated as a stack.
+     * @var array
+     */
+    private static $templates = array();
 
-    private $registered = array();
+    /**
+     * 'Global' variables for all templates.
+     * @var array
+     */
+    private static $variables = array();
 
+
+    private static $foreach_key = '';
+    private static $foreach_value = '';
+    private static $foreach_key_lvl2 = '';
+    private static $foreach_value_lvl2 = '';
+
+    /**
+     * Add a template to the stack.
+     *
+     * @param $view
+     * @throws \Exception
+     */
     protected function setView($view)
     {
         // @TODO: check config for templating
         $haml = new MtHaml\Environment('php');
 
-        // @TODO: get child class name
-        // @TODO: stocker les templates dans une pile ? avec leurs propre variable
+        if (strrpos($view, '/') === false) {
+            $className = explode('\\', get_called_class());
+            $controller = strtolower(str_replace('Controller', '', end($className)));
+            $template = FRONT_PATH . $controller . '/' . $view . '.haml';
+        } else {
+            $template = FRONT_PATH . $view . '.haml';
+        }
 
-        $template = FRONT_PATH . 'hello/home.haml';
+        if (!file_exists($template)) {
+            throw new Exception('[WalrusFrontController] requested template does not exist: ' . $template);
+        }
+
+
         $hamlCode = file_get_contents($template);
 
         if (!file_exists($template . '.php') || filemtime($template . '.php') != filemtime($template)) {
@@ -37,29 +68,64 @@ class WalrusFrontController
             touch($template.'.php', filemtime($template));
         }
 
-        $this->template = $template . '.php';
-        $this->execute();
+        $objTemplate = new Template();
+        $objTemplate->setName($view);
+        $objTemplate->setTemplate($template . '.php');
+
+        self::$templates[] = $objTemplate;
     }
 
-    protected function register($key, $var)
+    /**
+     * Add variables to the specified template or to all if not specified.
+     *
+     * @param $key
+     * @param $var
+     * @param null $tpl
+     * @throws \Exception
+     */
+    protected function register($key, $var, $tpl = null)
     {
         if (!isset($key) || !isset($var)) {
             throw new Exception('[WalrusFrontController] missing argument for function register');
         }
 
-        $this->registered[$key] = $var;
+        if ($tpl) {
+            foreach (self::$templates as $template) {
+                if ($template->getName() === $tpl) {
+                    $template->addVariable($key, $var);
+                    return;
+                }
+            }
+        } else {
+            self::$variables[$key] = $var;
+        }
     }
 
-    private function execute()
+    /**
+     * Display all templates in the stack order.
+     */
+    public static function execute()
     {
-        if (count($this->registered) > 0) {
-            foreach ($this->registered as $WALRUS_key => $WALRUS_value) {
-                ${$WALRUS_key} = $WALRUS_value;
+        if (count(self::$variables) > 0) {
+            foreach (self::$variables as self::$foreach_key => self::$foreach_value) {
+                ${self::$foreach_key} = self::$foreach_value;
             }
-            unset($WALRUS_key);
-            unset($WALRUS_value);
         }
 
-        require($this->template);
+        if (count(self::$templates) > 0) {
+            foreach (self::$templates as self::$foreach_key => self::$foreach_value) {
+
+                foreach (self::$foreach_value->getVariables() as self::$foreach_key_lvl2 => self::$foreach_value_lvl2) {
+                    ${self::$foreach_key_lvl2} = self::$foreach_value_lvl2;
+                }
+
+
+                require(self::$foreach_value->getTemplate());
+
+                foreach (self::$foreach_value->getVariables() as self::$foreach_key_lvl2 => self::$foreach_value_lvl2) {
+                    unset(${self::$foreach_key_lvl2});
+                }
+            }
+        }
     }
 }
