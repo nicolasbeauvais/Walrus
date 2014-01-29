ede3ace12f4e819da333630f99253642286ad3f1
ThomasHarper <thomasbentkowski@gmail.com>
2014-01-29 13:20:37 +0100

    Moving User model and controller to the right directories

---
+++ engine/controllers/UserController.php
@@ -0,0 +1,14 @@
+<?php
+/**
+ * Walrus Framework
+ * File maintained by: Thomas Bentkowski (Harper)
+ * Created: 13:12 29/01/14
+ */
+
+namespace engine\controllers;
+
+
+class UserController
+{
+
+}
---
+++ engine/models/User.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Walrus Framework
+ * File maintained by: Thomas Bentkowski (Harper)
+ * Created: 11:35 29/01/14
+ */
+
+namespace Walrus\models;
+
+use ActiveRecord;
+
+class User extends ActiveRecord\Model
+{
+    public $user;
+}
