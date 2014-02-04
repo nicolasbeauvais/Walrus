4d9cdd08598c544369729a885227f490cbd72ba1
FaustineLarmagna <f.larmagna@gmail.com>
2014-02-04 10:31:53 +0100

    modifs WalrusException

--- Walrus/core/WalrusException.php
+++ Walrus/core/WalrusException.php
@@ -26,16 +26,15 @@ use Exception;
 * }
 * catch(WalrusException $e)
 * {
-*	$error = new WalrusException;
-*	$error->MyError($e);
+*	$e->MyError();
 * }
 */
 
 class WalrusException extends Exception
 {
-	function MyError($e)
+	function MyError()
 	{
-		$errorInfo="\n".date('H:i:s d/m/Y')." | [line] -> ".$e->line." | [class] -> ".str_replace('.php','',basename($e->file))." | [Error] -> \"".$e->message."\"";
+		$errorInfo="\n".date('H:i:s d/m/Y')." | [line] -> ".$this->line." | [class] -> ".str_replace('.php','',basename($this->file))." | [Error] -> \"".$this->message."\"";
 		$logPath = __DIR__."/../../log.txt";
 		file_put_contents($logPath, $errorInfo, FILE_APPEND | LOCK_EX); // add the argument in the file but after with file append
 	}
