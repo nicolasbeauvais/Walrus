6577f2b9d3e1bafc68eff2bc25b7650134af7b30
Nicolas Beauvais <nicolas@bvs.email>
2014-02-05 23:12:44 +0100

    ENV: remove Readbean const from env.php

--- config/env.php
+++ config/env.php
@@ -19,27 +19,3 @@ define("ROOT_PATH", substr(dirname(__FILE__), 0, -7) . '/');
  * const FRONT_PATH, the templates directory, relative to ROOT_PATH.
  */
 define("FRONT_PATH", ROOT_PATH . 'www/templates' . '/');
-
-
-/** RedBeans ORM constants */
-
-/**
- * const RDBMS, Relational database management system : Mysql, PostgreSQL, SQLite...
- */
-define("RDBMS", "mysql");
-/**
- * const DB_HOST, Host for database in order to connect.
- */
-define("DB_HOST", "localhost");
-/**
- * const DB_NAME, Name of your database.
- */
-define("DB_NAME", "walrus_dev");
-/**
- * const DB_USER, Username of your database.
- */
-define("DB_USER", "root");
-/**
- * const DB_PWD, Password for your database's user.
- */
-define("DB_PWD", "root");
