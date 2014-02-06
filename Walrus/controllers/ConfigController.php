a019c3b9a0a8f9471d3c496e887a2fee998efae6
FaustineLarmagna <f.larmagna@gmail.com>
2014-02-06 15:37:58 +0100

    config form

--- Walrus/controllers/ConfigController.php
+++ Walrus/controllers/ConfigController.php
@@ -13,6 +13,17 @@ class ConfigController extends WalrusFrontController
 
     public function config()
     {
+      if ($_POST['config']) {
+	if (empty($_POST['dbName']) || empty($_POST['dbHost']) || empty($_POST['dbUser']) || empty($_POST['templating']) || empty($_POST['environment']) || empty($_POST['dbLanguage'])) {
+	  $this->setView('config');
+	  $error = "You didn't feed the information correctly. Please fix it."
+	}
+	else {
+	  $configInfo = $_POST;
+	  //appel a la fonction qui feed la config. avec en parametre $configInfo
+	}
+      }
+      else
         $this->setView('config');
     }
 }
