cc5212041c861ccaa1236c92c858f5afdccef494
Nicolas Beauvais <n.beauvais78@gmail.com>
2014-02-08 00:38:25 +0100

    WalrusAPI: add comment block

--- Walrus/core/WalrusAPI.php
+++ Walrus/core/WalrusAPI.php
@@ -5,11 +5,14 @@
  * File maintained by: Nicolas Beauvais (E-Wok)
  * Created: 00:29 08/02/14
  */
- 
 
 namespace Walrus\core;
 
+/**
+ * Class WalrusAPI
+ * @package Walrus\core
+ */
+class WalrusAPI
+{
 
-class WalrusAPI {
-
-} 
+}
