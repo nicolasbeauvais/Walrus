efc29ea522894c5476f4512522038c0ce9bc5874
Nicolas Beauvais <n.beauvais78@gmail.com>
2014-02-08 12:59:37 +0100

    WalrusAPI: add severals tools and automation for API

--- Walrus/core/WalrusAPI.php
+++ Walrus/core/WalrusAPI.php
@@ -16,13 +16,47 @@ use Exception;
  */
 class WalrusAPI
 {
+    /**
+     * @var array
+     */
+    private static $allow = array();
+
+    /**
+     * @var string
+     */
+    private static $contentType = "application/json";
+
+    /**
+     * @var array
+     */
+    private static $request = array();
+
+    /**
+     * @var string
+     */
+    private static $method = "";
+
+    /**
+     * @var int
+     */
+    private static $code = 200;
+
+    /**
+     * Array of instancied controllers
+     */
+    private $models;
+
+    /**
+     * Array of instancied controllers
+     */
+    private $controllers;
 
     /**
      * Init an API process
      */
     public static function init()
     {
-        // @TODO: API init
+        self::inputs();
     }
 
     /**
@@ -31,7 +65,7 @@ class WalrusAPI
      */
     public static function execute($data)
     {
-
+        self::response($data, self::$code);
     }
 
     /**
@@ -88,5 +122,137 @@ class WalrusAPI
         $this->models[$modelClass] = $modelInstance;
 
         return $modelInstance;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getReferer()
+    {
+        return $_SERVER['HTTP_REFERER'];
+    }
+
+    /**
+     * @param $data
+     * @param $status
+     */
+    private static function response($data, $status)
+    {
+        self::$code = ($status) ? $status : 200;
+        self::setHeaders();
+        echo JSON_encode($data);
+        exit;
+    }
+
+    /**
+     * @return mixed
+     */
+    private static function getStatusMessage()
+    {
+        $status = array(
+            100 => 'Continue',
+            101 => 'Switching Protocols',
+            200 => 'OK',
+            201 => 'Created',
+            202 => 'Accepted',
+            203 => 'Non-Authoritative Information',
+            204 => 'No Content',
+            205 => 'Reset Content',
+            206 => 'Partial Content',
+            300 => 'Multiple Choices',
+            301 => 'Moved Permanently',
+            302 => 'Found',
+            303 => 'See Other',
+            304 => 'Not Modified',
+            305 => 'Use Proxy',
+            306 => '(Unused)',
+            307 => 'Temporary Redirect',
+            400 => 'Bad Request',
+            401 => 'Unauthorized',
+            402 => 'Payment Required',
+            403 => 'Forbidden',
+            404 => 'Not Found',
+            405 => 'Method Not Allowed',
+            406 => 'Not Acceptable',
+            407 => 'Proxy Authentication Required',
+            408 => 'Request Timeout',
+            409 => 'Conflict',
+            410 => 'Gone',
+            411 => 'Length Required',
+            412 => 'Precondition Failed',
+            413 => 'Request Entity Too Large',
+            414 => 'Request-URI Too Long',
+            415 => 'Unsupported Media Type',
+            416 => 'Requested Range Not Satisfiable',
+            417 => 'Expectation Failed',
+            500 => 'Internal Server Error',
+            501 => 'Not Implemented',
+            502 => 'Bad Gateway',
+            503 => 'Service Unavailable',
+            504 => 'Gateway Timeout',
+            505 => 'HTTP Version Not Supported');
+        return ($status[self::$code]) ? $status[self::$code] : $status[500];
+    }
+
+    /**
+     * @return mixed
+     */
+    private static function getRequestMethod()
+    {
+        return $_SERVER['REQUEST_METHOD'];
+    }
+
+    /**
+     *
+     */
+    private static function inputs()
+    {
+        switch(self::getRequestMethod()){
+            case "POST":
+                self::$request = self::cleanInputs($_POST);
+                break;
+            case "GET":
+            case "DELETE":
+                self::$request = self::cleanInputs($_GET);
+                break;
+            case "PUT":
+                parse_str(file_get_contents("php://input"), self::$request);
+                self::$request = self::cleanInputs(self::$request);
+                break;
+            default:
+                self::response('', 406);
+                break;
+        }
+    }
+
+    /**
+     * @param $data
+     *
+     * @return array|string
+     */
+    private static function cleanInputs($data)
+    {
+        $clean_input = array();
+        if (is_array($data)) {
+            foreach ($data as $k => $v) {
+                $clean_input[$k] = self::cleanInputs($v);
+            }
+        } else {
+            if (get_magic_quotes_gpc()) {
+                $data = trim(stripslashes($data));
+            }
+            $data = strip_tags($data);
+            $clean_input = trim($data);
+        }
+        return $clean_input;
+    }
+
+    /**
+     *
+     */
+    private static function setHeaders()
+    {
+        header("HTTP/1.1 " . self::$code . " " . self::getStatusmessage());
+        header("Content-Type:" . self::$contentType);
     }
 }
