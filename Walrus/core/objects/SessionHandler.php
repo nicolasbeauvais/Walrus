c8a03fda3cf42f4a6fb314f725a246f86ab69277
Nicolas Beauvais <n.beauvais78@gmail.com>
2014-02-08 17:39:29 +0100

    Home front: prepare for long polling front test

---
+++ Walrus/core/objects/SessionHandler.php
@@ -0,0 +1,15 @@
+<?php
+
+/**
+ * Walrus Framework
+ * File maintained by: Nicolas Beauvais (E-Wok)
+ * Created: 14:20 08/02/14
+ */
+ 
+
+namespace Walrus\core\objects;
+
+
+class SessionHandler {
+
+} 
