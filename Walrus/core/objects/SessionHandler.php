cb86dada69c52926aeb40b3cb54d02bbb872f1ad
Nicolas Beauvais <n.beauvais78@gmail.com>
2014-02-08 17:42:28 +0100

    SessionHandler object: total refactor, now use redbeanORM

--- Walrus/core/objects/SessionHandler.php
+++ Walrus/core/objects/SessionHandler.php
@@ -5,11 +5,13 @@
  * File maintained by: Nicolas Beauvais (E-Wok)
  * Created: 14:20 08/02/14
  */
- 
 
 namespace Walrus\core\objects;
 
+use R;
 
 class SessionHandler {
 
-} 
+        return true;
+    }
+}
