d62fbc026a2b6177313763e1c303c37dd2ef4b3a
Nicolas Beauvais <n.beauvais78@gmail.com>
2014-02-08 01:06:51 +0100

    Polling controller: working API call

--- engine/api/pollingController.php
+++ engine/api/pollingController.php
@@ -5,16 +5,19 @@
  * File maintained by: Nicolas Beauvais (E-Wok)
  * Created: 00:09 08/02/14
  */
- 
 
 namespace engine\api;
 
+use Walrus\core\WalrusAPI;
 
 /**
  * Class PollingController
  * @package engine\api
  */
-class PollingController
+class PollingController extends WalrusAPI
 {
+    public function run()
+    {
 
+    }
 }
