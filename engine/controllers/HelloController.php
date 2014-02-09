a861883418ff827fe76336cb3d0ed5376e43cfe4
Nicolas Beauvais <n.beauvais78@gmail.com>
2014-02-09 19:53:37 +0100

    Walrus: sanitize yml file + hello walrus controller

--- engine/controllers/HelloController.php
+++ engine/controllers/HelloController.php
@@ -4,32 +4,15 @@ namespace engine\controllers;
 
 use Walrus\core\WalrusFrontController as WalrusFrontController;
 
-class HomeController extends WalrusFrontController
+/**
+ * Class HelloController
+ * @package engine\controllers
+ */
+class HelloController extends WalrusFrontController
 {
 
     public function run()
     {
-        if (isset($_POST['type'])) {
-            if ($_POST['type'] === 'login') {
-                $this->model('user')->login();
-            } elseif ($_POST['type'] === 'signup') {
-                $noError = $this->model('user')->signup();
-                if ($noError === false) {
-                    $error = "Pseudo already taken.";
-                    $this->register('error', $error);
-                }
-            }
-        }
-
-        if (isset($_SESSION['name'])) {
-            $this->reroute('dashboard', 'run');
-        }
-
-        $this->skeleton('_skeleton_home');
-    }
-
-    public function testRoute($param1, $param2, $param3, $param4 = 'coucou')
-    {
-        var_dump($param1, $param2, $param3, $param4);
+        echo 'Hello Walrus!';
     }
 }
