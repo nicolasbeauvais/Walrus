5746343ffbd1bf4ffe23c95dab7f717611dc8685
Nicolas Beauvais <nicolas@bvs.email>
2014-02-09 01:11:23 +0100

    Script.js: longpolling is ok, need to be move in walrus.js with less step to do for user

--- www/assets/javascript/script.js
+++ www/assets/javascript/script.js
@@ -7,15 +7,44 @@ $('#post').click(function () {
     $('#post-pop').toggleClass('expand');
 });
 
-$('#post-pop form').submit(function () {
+$('#post-pop form').submit(function (e) {
     $.ajax({
         type: 'POST',
         url: $(this).attr('action'),
         data: $(this).serialize(),
         success: function () {
             $('#post-pop').toggleClass('expand');
+            var node = $('#userBar').find('.infos .stat:first .value');
+            node[0].innerHTML = parseInt(node[0].innerHTML) + 1;
         }
     });
 
+    e.preventDefault();
+    e.stopPropagation();
     return false;
 });
+
+
+var polling = {};
+
+function poll() {
+    polling = $.ajax({
+        type: "POST",
+        url: 'api/polling/run/'
+    });
+
+    polling.done(function (data) {
+        if (data.status === 200) {
+            data = data.data;
+
+            if (!data) { return; }
+            if (data.posts) {
+                console.log(data.posts);
+            }
+        }
+
+        setTimeout(poll, 1000);
+    });
+}
+
+poll();
