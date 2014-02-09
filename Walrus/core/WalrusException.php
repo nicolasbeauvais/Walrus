768a768efa62aa2a6a0a2ac4f0359ceddce0a1dd
Nicolas Beauvais <n.beauvais78@gmail.com>
2014-02-09 18:22:10 +0100

    WalrusException: Method doc + refactor PSR-2

--- Walrus/core/WalrusException.php
+++ Walrus/core/WalrusException.php
@@ -17,10 +17,16 @@ use Exception;
  */
 class WalrusException extends Exception
 {
-	function MyError()
-	{
-		$errorInfo="\n".date('H:i:s d/m/Y')." | [line] -> ".$this->line." | [class] -> ".str_replace('.php','',basename($this->file))." | [Error] -> \"".$this->message."\"";
-		$logPath = __DIR__."/../../log.txt";
-		file_put_contents($logPath, $errorInfo, FILE_APPEND | LOCK_EX); // add the argument in the file but after with file append
-	}
+    // @TODO: refactor
+
+    /**
+     * WalrusException format the exception and insert it in the log file.
+     */
+    public function errorHandler()
+    {
+        $errorInfo= "\n" . date('H:i:s d/m/Y')." | [line] -> " . $this->line . " | [class] -> "
+            . str_replace('.php', '', basename($this->file)) . " | [Error] -> \"" . $this->message . "\"";
+        $logPath = __DIR__."/../../log.txt";
+        file_put_contents($logPath, $errorInfo, FILE_APPEND | LOCK_EX);
+    }
 }
