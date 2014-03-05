b3e5f32972abe624825c2040e577f0de0b137a91
Nicolas Beauvais <n.beauvais78@gmail.com>
2014-03-05 07:59:48 +0100

    WalrusMonitoring: moves Walrus view scripts in there own directory

--- Walrus/templates/monitoring/e2.php
+++ Walrus/templates/monitoring/e2.php
@@ -545,8 +545,8 @@
 
 <script>
     <?php
-        require_once('jquery.js');
-        require_once('e2-scripts.js');
+        require_once(ROOT_PATH . 'Walrus/templates/scripts/jquery.js');
+        require_once(ROOT_PATH . 'Walrus/templates/scripts/e2-scripts.js');
     ?>
 
     (function(){
