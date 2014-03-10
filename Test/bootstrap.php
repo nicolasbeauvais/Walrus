685624de3e3b9d900d1b40629809c141d00af007
Nicolas Beauvais <n.beauvais78@gmail.com>
2014-03-10 07:53:22 +0100

    Test bootstrap: create test directory if it doesn't exist

--- Test/bootstrap.php
+++ Test/bootstrap.php
@@ -6,10 +6,6 @@
  * Created: 08:00 07/03/14
  */
 
-use Walrus\core\WalrusCLI as WalrusCLI;
-
-// constante
-
 /**
  * const ROOT_PATH, the root directory of walrus.
  */
@@ -21,7 +17,17 @@ define("ROOT_PATH", substr(dirname(__FILE__), 0, -4));
 define("TESTBOX_PATH", ROOT_PATH . 'testbox\\');
 
 // autoload
-
 require_once('Walrus/core/WalrusAutoload.php');
-
 new \Walrus\core\WalrusAutoload();
+
+// directory initialisation
+if (!file_exists(ROOT_PATH . '\Test\testbox')) {
+    try {
+        $filer = new Walrus\core\WalrusFileManager(ROOT_PATH . '\Test');
+        $filer->folderCreate('testbox', 777);
+    } catch (Exception $exception) {
+        echo 'Failed directory initilisation' . "\r\n";
+        echo $exception->getMessage();
+        die;
+    }
+}
