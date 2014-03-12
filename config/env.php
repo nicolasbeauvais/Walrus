5f9f860b2991354f1d5981373ad8c64367080a73
Nicolas Beauvais <n.beauvais78@gmail.com>
2014-03-12 09:30:40 +0100

    ENV: use DIRECTORY_SEPARATOR const

--- config/env.php
+++ config/env.php
@@ -14,17 +14,17 @@ define("WALRUS_VERSION", '1.1.0b');
 /**
  * const APP_PATH, the acces directory of walrus "./www/".
  */
-define("APP_PATH", dirname(__FILE__) . '/');
+define("APP_PATH", dirname(__FILE__) . DIRECTORY_SEPARATOR);
 
 /**
  * const ROOT_PATH, the root directory of walrus.
  */
-define("ROOT_PATH", substr(dirname(__FILE__), 0, -7) . '/');
+define("ROOT_PATH", substr(dirname(__FILE__), 0, -7) . DIRECTORY_SEPARATOR);
 
 /**
  * const FRONT_PATH, the templates directory, relative to ROOT_PATH.
  */
-define("FRONT_PATH", ROOT_PATH . 'templates' . '/');
+define("FRONT_PATH", ROOT_PATH . 'templates' . DIRECTORY_SEPARATOR);
 
 /**
  * const START_TIME, microtime for the walrus launch
