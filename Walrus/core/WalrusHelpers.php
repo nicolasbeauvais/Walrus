5ee6687f1c56dd21552ef34c5c9abd7066824323
Nicolas Beauvais <nicolas@bvs.email>
2014-03-27 22:09:11 +0100

    WalrusHelpers: refactor, tested

--- Walrus/core/WalrusHelpers.php
+++ Walrus/core/WalrusHelpers.php
@@ -27,8 +27,9 @@ class WalrusHelpers
      * A helper name should pass this regex: [A-Za-z_]+
      */
     private static $helpers = array(
-        'Url',
-        'Form'
+        'Url' => array('class' => 'Url','type' => 0),
+        'Form' => array('class' => 'Form', 'type' => 0),
+        'Tag' => array('class' => 'Tag', 'type' => 1)
     );
 
     /**
@@ -79,13 +80,33 @@ class WalrusHelpers
     }
 
     /**
-     * Create each helper object.
+     * Return a helper class
+     *
+     * @param string $helper the wanted helper name
+     * @param bool $newInstance return a new instance or previously instancied if exist
+     *
+     * @throws WalrusException in case the required helper doesn't exist
+     *
+     * @return helper instance
      */
-    private static function create()
+    public static function getHelper($helper, $newInstance = true)
     {
-        foreach (self::$helpers as $helper) {
-            self::$helpersInstances[$helper] = self::instanceClass($helper);
+        if (!array_key_exists($helper, self::$helpers)) {
+            throw new WalrusException('The helper: ' . $helper . ' doesn\'t exist or not registered to WalrusHelpers');
         }
+
+        if ((isset(self::$helpersInstances[$helper]) && $newInstance == false)
+            && self::test(self::$helpers[$helper]['class'], 'back')) {
+            return self::$helpersInstances[$helper];
+        } else {
+            $instance = self::instanceClass($helper);
+
+            if (!isset(self::$helpersInstances[$helper]) || $newInstance) {
+                self::$helpersInstances[$helper] = $instance;
+            }
+        }
+
+        return self::instanceClass($helper);
     }
 
     /**
@@ -96,24 +117,88 @@ class WalrusHelpers
      */
     private static function instanceClass($helper)
     {
-        $classWithNamespace = self::$helpersNamespace . $helper;
-        return new $classWithNamespace();
+        $instance = self::$helpersNamespace . $helper;
+        return new $instance();
     }
 
     /**
-     * Add a helper to the WalrusHelpers class.
+     * Return all helper with front-end type to use in WalrusFrontController
+     */
+    public static function create()
+    {
+        foreach (self::$helpers as $helper) {
+            if (isset(self::$helpersInstances[$helper['class']])) {
+                continue;
+            }
+            self::$helpersInstances[$helper['class']] = self::instanceClass($helper['class']);
+        }
+
+        return self::$helpersInstances;
+    }
+
+    /**
+     * Test if a helper can be called by the function.
      *
-     * @param $class the class name of the helper
+     * @param int $helper can be 0, 1 or 2
+     * @param string $frontOrBack can be Front or Back
+     *
+     * @throws WalrusException if the helper have a bad type
+     *
+     * @return bool
+     */
+    private static function test($helper, $frontOrBack)
+    {
+        $type = self::$helpers[$helper];
+
+        $type = abs((int)$type);
+        if ($type > 2) {
+            throw new WalrusException('The Helper ' . $helper .' hasn\'t a correct type (0, 1 or 2)');
+        }
+
+        if ($frontOrBack == 'front') {
+            return $type !== 1;
+        } elseif ($frontOrBack == 'back') {
+            return $type !== 2;
+        } else {
+            throw new WalrusException('$frontOrBack is set to a bad value, must be the string "front" or "back"');
+        }
+    }
+
+    /**
+     * Register a helper to the WalrusHelpers class.
+     *
+     * $type values:
+     * 0: accessibility from backend and frontend
+     * 1: accessibility from backend only (controllers/models)
+     * 2: accessibility from frontend only (views/templates)
+     *
+     * @param string $class the class name of the helper
+     * @param int $type security type
      *
      * @throws WalrusException if a helper with this $class name already exist
      */
-    private static function registerHelper($class)
+    public static function registerHelper($class, $type = 0)
     {
-        if (!in_array($class, self::$helpers)) {
-            self::$helpers[] = $class;
+        $type = abs((int)$type);
+        if ($type > 2) {
+            throw new WalrusException('A Walrus Helper must have a correct type: 0, 1 or 2');
+        }
+
+        if (!array_key_exists($class, self::$helpers)) {
+            self::setHelper($class, $type);
         } else {
             throw new WalrusException('A Walrus helper with the name ' . $class . ' already exist');
         }
+    }
 
+    /**
+     * Add a new Helper to the helpers array
+     *
+     * @param string $class the class name of the helper
+     * @param int $type security type
+     */
+    private static function setHelper($class, $type)
+    {
+        self::$helpers[$class] = array('class' => $class, 'type' => $type);
     }
 }
