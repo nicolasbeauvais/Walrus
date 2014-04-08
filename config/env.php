bec09240e5f801d3d23199b9f2c8908846f51756
Nicolas Beauvais <n.beauvais78@gmail.com>
2014-04-08 13:37:42 +0200

    Env: no more const, only $_ENV

--- config/env.php
+++ config/env.php
@@ -2,31 +2,21 @@
 
 /**
  * Walrus Framework
- * File maintained by: Nicolas Beauvais (E-Wok)
- * Created: 08:06 04/02/14
+ * File maintained by: Nicolas Beauvais
+ * Created: 08:20 10/04/14
  */
 
-/**
- * const WALRUS_VERSION, the current version of Walrus.
- */
-define("WALRUS_VERSION", '1.0.0b');
+// const VERSION, the current version of Walrus.
+$_ENV['W']['VERSION'] = '1.0.0b';
 
-/**
- * const APP_PATH, the acces directory of walrus "./www/".
- */
-define("APP_PATH", dirname(__FILE__) . DIRECTORY_SEPARATOR);
+// const APP_PATH, the acces directory of walrus "./www/".
+$_ENV['W']['APP_PATH'] = dirname(__FILE__) . DIRECTORY_SEPARATOR;
 
-/**
- * const ROOT_PATH, the root directory of walrus.
- */
-define("ROOT_PATH", substr(dirname(__FILE__), 0, -7) . DIRECTORY_SEPARATOR);
+// const ROOT_PATH, the root directory of walrus.
+$_ENV['W']['ROOT_PATH'] = substr(dirname(__FILE__), 0, -7) . DIRECTORY_SEPARATOR;
 
-/**
- * const FRONT_PATH, the templates directory, relative to ROOT_PATH.
- */
-define("FRONT_PATH", ROOT_PATH . 'templates' . DIRECTORY_SEPARATOR);
+// const FRONT_PATH, the templates directory, relative to ROOT_PATH.
+$_ENV['W']['FRONT_PATH'] = $_ENV['W']['ROOT_PATH'] . 'templates' . DIRECTORY_SEPARATOR;
 
-/**
- * const START_TIME, microtime for the walrus launch
- */
-define("START_TIME", microtime(true));
+// const START_TIME, microtime for the walrus launch
+$_ENV['W']['START_TIME'] = microtime(true);
