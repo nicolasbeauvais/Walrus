9e0c12e494c5656bfcf28e30678b0f5763f01f0a
Nicolas Beauvais <n.beauvais78@gmail.com>
2014-04-23 00:17:43 +0200

    WalrusCompile: all methods done

--- Walrus/core/WalrusCompile.php
+++ Walrus/core/WalrusCompile.php
@@ -53,14 +53,22 @@ class WalrusCompile
 
     /**
      * Main function to call to get an instance of WalrusCompile.
+     *
+     * @param bool $deploy
+     *
      * @return WalrusCompile
      */
-    public static function launch()
+    public static function launch($deploy = false)
     {
         if (!isset(self::$instance)) {
             self::$instance = new self;
             self::construct();
-            self::setup();
+
+            if (!$deploy) {
+                self::setup();
+            } else {
+                self::compileForProduction();
+            }
         }
 
         return self::$instance;
@@ -97,10 +105,10 @@ class WalrusCompile
         }
 
         if ($_ENV['W']['environment'] == 'development') { // if dev
-                self::compile();
+            self::compile();
         } else {
             require_once($_ENV['W']['ROOT_PATH'] . 'config/config.php');
-            require_once($_ENV['W']['ROOT_PATH'] . 'config/conpiled.php');
+            require_once($_ENV['W']['ROOT_PATH'] . 'config/compiled.php');
         }
 
         return true;
@@ -108,6 +116,8 @@ class WalrusCompile
 
     /**
      * Check and compile if necessary all config files, for development environment only
+     *
+     * @return array of existing YAML files
      */
     private static function compile()
     {
@@ -116,9 +126,13 @@ class WalrusCompile
         $filer = new WalrusFileManager(self::$configPath);
         $tree = $filer->getFolderTree();
 
+        $_ENV['W']['development']['nb_file_compiled'] = 0;
+
         if (isset($tree['compiled'])) {
             unset($tree['compiled']);
         }
+
+        $completeTree = $tree;
 
         // create directories if they don't exist
         foreach (self::$entities as $entity) {
@@ -133,6 +147,8 @@ class WalrusCompile
             // check each file
             $filer->setCurrentElem($entityPlu);
             $entityTree = $filer->getElements();
+
+            $completeTree[$entityPlu] = $entityTree;
 
             foreach ($entityTree as $YAMLFile) {
 
@@ -181,20 +197,24 @@ class WalrusCompile
                         $filer->setCurrentElem($filer->pathJoin('compiled', $entityPlu, $PHPFile));
                     }
 
-                    //touch($filer->getCurrentElem());
+                    $_ENV['W']['development']['nb_file_compiled']++;
+
+                    touch($filer->getCurrentElem());
                 }
             }
 
             $filer->setCurrentElem('');
         }
 
-        self::load();
+        self::load($completeTree);
     }
 
     /**
      * Load all compiled files
+     *
+     * @param array $YAMLTree a tree of th existing yaml conf files
      */
-    private static function load()
+    private static function load($YAMLTree)
     {
         $filer = new WalrusFileManager(self::$compiledPath);
 
@@ -207,20 +227,95 @@ class WalrusCompile
             }
 
             foreach ($entity as $file) {
+
+                $fileName = substr($file, 0, -4);
+
                 $filer->setCurrentElem($filer->pathJoin($dir, $file));
-                $content = $filer->getFileContent();
-                $_ENV['W'][$dir] = unserialize($content);
+
+                if (!in_array($fileName . '.yml', $YAMLTree[$dir])) {
+                    $filer->deleteCurrent();
+                } else {
+                    $content = $filer->getFileContent();
+                    $_ENV['W'][$dir] = unserialize($content);
+                }
             }
         }
     }
 
     /**
-     * Compile all yaml files into one compiled.php files (used by CLI, for production environment only).
+     * Compile all php files into one compiled.php files (used by CLI, for production environment only).
      */
     private static function compileForProduction()
     {
-        foreach (self::$entities as $entitie) {
-            //@TODO
+        $filer = new WalrusFileManager($_ENV['W']['ROOT_PATH']);
+
+        $filer->setCurrentElem($filer->pathJoin('Walrus', 'core', 'sample', 'compiled.sample'));
+        $sample = $filer->getFileContent();
+
+        $filer->setCurrentElem($filer->pathJoin('config', 'compiled'));
+        $tree = $filer->getElements(true);
+
+        $compiled = '';
+
+        foreach ($tree as $dir => $entity) {
+
+            if (empty($entity)) {
+                continue;
+            }
+
+            $compiled .= "\r\n" . '// ' . $dir . "\r\n";
+
+            foreach ($entity as $file) {
+                $filer->setCurrentElem($filer->pathJoin('config', 'compiled', $dir, $file));
+                $content = $filer->getFileContent();
+                $phpArray = unserialize($content);
+
+                $index = '$_ENV[\'W\'][\'' . $dir . '\']';
+                $vars = self::convertPHPArrayToPHPString($index, $phpArray);
+
+                $compiled .= $vars;
+            }
         }
+
+        $filer->setCurrentElem('');
+
+        if (!file_exists($filer->filerPathJoin('config', 'compiled.php'))) {
+            $filer->setCurrentElem('config');
+            $filer->fileCreate('compiled.php');
+        }
+
+        $sample = str_replace('%date%', date('Y/m/d H:i:s'), $sample);
+        $sample = str_replace('%content%', $compiled, $sample);
+
+        $filer->setCurrentElem($filer->pathJoin('config', 'compiled.php'));
+        $filer->changeFileContent($sample);
+    }
+
+    /**
+     * Convert a PHP array to a valid PHP file
+     *
+     * @param string $index the output index
+     * @param array $array a valid PHP array
+     *
+     * @return string
+     */
+    public static function convertPHPArrayToPHPString($index, $array)
+    {
+        $phpString = '';
+
+        foreach ($array as $key => $row) {
+
+            $itemIndex = $index . '[\'' . $key . '\']';
+
+            if (is_array($row)) {
+                $phpString .= self::convertPHPArrayToPHPString($itemIndex, $row);
+            } elseif (is_string($row)) {
+                $phpString .= $itemIndex . ' = "' . $row . '";' . "\r\n";
+            } elseif (is_object($row)) {
+                $phpString .= $itemIndex . ' = "' . serialize($row) . '";' . "\r\n";
+            }
+        }
+
+        return $phpString;
     }
 }
