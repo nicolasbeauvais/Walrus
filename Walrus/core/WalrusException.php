3c5e892e5f63464ba93fba016573c4953644c2a3
Nicolas Beauvais <nicolas@bvs.email>
2014-06-17 18:49:49 +0200

    WalrusException: use the caller class name to prefix the exception

--- Walrus/core/WalrusException.php
+++ Walrus/core/WalrusException.php
@@ -19,6 +19,27 @@ use Walrus\core\WalrusMonitoring;
 class WalrusException extends Exception
 {
     /**
+     * Exception constructor
+     */
+    public function __construct($name)
+    {
+        $trace = debug_backtrace();
+
+        if (isset($trace[1]) && isset($trace[1]['class'])) {
+            if (strpos('\\', $trace[1]['class']) != -1) {
+                $namespace = explode('\\', $trace[1]['class']);
+                $prefix = end($namespace);
+            } else {
+                $prefix = $trace[1]['class'];
+            }
+
+            $name = $prefix . ': ' . $name;
+        }
+
+        $this->message = $name;
+    }
+
+    /**
      * Handle a WalrusException to display it in the exception debuger
      */
     public function handle()
