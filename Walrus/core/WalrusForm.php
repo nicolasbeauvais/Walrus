6856c35e225d5b4d109f4ba5aa8685ec2826b4cf
Nicolas Beauvais <nicolas@bvs.email>
2014-06-24 10:02:00 +0200

    WalrusForm: add field validation, display errors and new checks

--- Walrus/core/WalrusForm.php
+++ Walrus/core/WalrusForm.php
@@ -33,6 +33,11 @@ class WalrusForm
     private $fields;
 
     /**
+     * @var array
+     */
+    private $errors;
+
+    /**
      * @param $name
      * @throws WalrusException
      */
@@ -51,6 +56,194 @@ class WalrusForm
         $this->name = $name;
         $this->form = $form['form'];
         $this->fields = $form['fields'];
+    }
+
+    /**
+     * Check if a form as been submitted.
+     * If a submitted form as been detected:
+     *   - check each field validation
+     *   - return an array of errors OR reroute if $controller & $action param are set, else return true
+     * Else
+     *   - return null
+     *
+     *
+     * @param string $controller a controller name
+     * @param string $action an action of the controller
+     * @param array $param an array of the parameter to pass to the controller
+     *
+     * @return mixed
+     */
+    public function check($controller = null, $action = null, $param = null)
+    {
+        $errors = array();
+        $data = $this->form['method'] == 'POST' ? $_POST : $_GET;
+
+        // No form have been send
+        if (empty($data)) {
+            return null;
+        }
+
+        // check each field
+        foreach ($this->fields as $name => $field) {
+
+            $check = isset($field['check']) ? $field['check'] : array();
+            $isKnown = in_array($field['type'], array('select', 'radio', 'checkbox'));
+
+            if (empty($field['check']) && !$isKnown) {
+                continue;
+            }
+
+            if ($isKnown && isset($data[$name])) {
+
+                // process function
+                if (isset($field['function'])) {
+                    $cb = explode(':', $field['function']);
+                    $field['options'] = WalrusRouter::reroute($cb[0], $cb[1]);
+                }
+
+                // if one simple value
+                if (!is_array($data[$name])) {
+                    $elements = array($data[$name]);
+                } else {
+                    $elements = $data[$name];
+                }
+
+                if (!isset($field['options'])) {
+                    continue;
+                }
+
+                // check input integrity
+                foreach ($elements as $element) {
+                    if (!array_key_exists($element, $field['options'])) {
+                        $errors[$name] = WalrusI18n::get('errors', 'messages', 'invalid', array(
+                            'attribute' => $name
+                        ));
+                    }
+                }
+
+                if (isset($check['max_selected']) && count($elements) > $check['max_selected']) {// check max_selected
+                    $errors[$name] = WalrusI18n::get('errors', 'messages', 'max_selected', array(
+                        'attribute' => $name,
+                        'count' => $check['max_selected']
+                    ));
+                }
+
+                if (isset($check['min_selected']) && count($elements) < $check['min_selected']) {// check max_selected
+                    $errors[$name] = WalrusI18n::get('errors', 'messages', 'min_selected', array(
+                        'attribute' => $name,
+                        'count' => $check['min_selected']
+                    ));
+                }
+
+                if (isset($check['selected']) && count($elements) != $check['selected']) {// check max_selected
+                    $errors[$name] = WalrusI18n::get('errors', 'messages', 'selected', array(
+                        'attribute' => $name,
+                        'count' => $check['selected']
+                    ));
+                }
+
+            } elseif (!isset($check['required'])) {
+                $check['required'] = true;
+            }
+
+            // @TODO: check validate field (regex), function caller, email check
+
+            // check required
+            if (isset($check['required']) && $check['required'] == true && !(isset($data[$name]))) {// check required
+                $errors[$name] = WalrusI18n::get('errors', 'messages', 'required', array('attribute' => $name));
+                continue;
+            }
+            if (isset($check['blank']) && $check['blank'] == true && isset($data[$name])) {// check blank
+                $errors[$name] = WalrusI18n::get('errors', 'messages', 'blank', array('attribute' => $name));
+                continue;
+            }
+            if (isset($check['empty']) && $check['empty'] == true && empty($data[$name])) {// check empty
+                $errors[$name] = WalrusI18n::get('errors', 'messages', 'empty', array('attribute' => $name));
+                continue;
+            }
+            if (isset($check['equal_to'])) {// check equal_to
+
+                $equal_to = $check['equal_to'];
+
+                if (strpos($check['equal_to'], '%') === 0) {
+                    $equal_to = substr($check['equal_to'], 1, strlen($check['equal_to']));
+                    $check['equal_to'] = isset($data[$equal_to]) ? $data[$equal_to] : null;
+                }
+
+                if ($check['equal_to'] !== $data[$name]) {
+                    $errors[$name] = WalrusI18n::get('errors', 'messages', 'equal_to', array(
+                        'attribute' => $name,
+                        'value' => $equal_to
+                    ));
+                }
+            }
+
+            if (isset($check['even']) && $check['even'] == true && $data[$name] % 2 !== 0) {// check even
+                $errors[$name] = WalrusI18n::get('errors', 'messages', 'even', array('attribute' => $name));
+            }
+            if (isset($check['greater_than']) && $check['greater_than'] <= $data[$name]) {// check greater_than
+                $errors[$name] = WalrusI18n::get('errors', 'messages', 'greater_than', array(
+                    'attribute' => $name,
+                    'count' => $check['greater_than']
+                ));
+            }
+            if (isset($check['greater_than_or_equal_to'])
+                && $check['greater_than_or_equal_to'] < $data[$name]) {// check greater_than_or_equal_to
+                $errors[$name] = WalrusI18n::get(
+                    'errors',
+                    'messages',
+                    'greater_than_or_equal_to',
+                    array($check['greater_than_or_equal_to'])
+                );
+            }
+            if (isset($check['greater_than']) && $check['greater_than'] <= $data[$name]) {// check greater_than
+                $errors[$name] = WalrusI18n::get('errors', 'messages', 'greater_than');
+            }
+            if (isset($check['number']) && $check['number'] == true && !is_numeric($data[$name])) {// check numb
+                $errors[$name] = WalrusI18n::get('errors', 'messages', 'not_a_number', array('attribute' => $name));
+            }
+            if (isset($check['integer']) && $check['integer'] == true && !is_int($data[$name])) {// check int
+                $errors[$name] = WalrusI18n::get('errors', 'messages', 'not_an_integer', array('attribute' => $name));
+            }
+            if (isset($check['odd']) && $check['odd'] == true && $data[$name] % 2 == 0) {// check odd
+                $errors[$name] = WalrusI18n::get('errors', 'messages', 'odd', array('attribute' => $name));
+            }
+            if (isset($check['max']) && $check['max'] == true && $data[$name] > $check['max']) {// check max
+                $errors[$name] = WalrusI18n::get('errors', 'messages', 'max', array(
+                    'attribute' => $name,
+                    'count' => $check['max']
+                ));
+            }
+            if (isset($check['min']) && $check['min'] == true && $data[$name] < $check['min']) {// check min
+                $errors[$name] = WalrusI18n::get('errors', 'messages', 'min', array(
+                    'attribute' => $name,
+                        'count' => $check['min']
+                ));
+            }
+            if (isset($check['length']) && $check['length'] == true
+                && strlen($data[$name]) !== $check['length']) {// check length
+                $errors[$name] = WalrusI18n::get('errors', 'messages', 'length', array(
+                    'attribute' => $name,
+                    'count' => $check['min']
+                ));
+            }
+        }
+
+        if ($_ENV['W']['is_ajax']) {
+            echo JSON_encode($errors);
+            die;
+        }
+
+        if (empty($errors)) {
+            if ($controller && $action) {
+                WalrusRouter::reroute($controller, $action, $param);
+            }
+            return true;
+        }
+
+        $this->errors = $errors;
+
+        return $errors;
     }
 
     /**
@@ -94,11 +287,11 @@ class WalrusForm
             $row = array();
 
             // separate known data and attributes
-            $required = isset($field['required']) ? $field['required'] : null;
-            $validate = isset($field['validate']) ? $field['validate'] : null;
+            $check = isset($field['check']) ? $field['check'] : null;
             $options = isset($field['options']) ? $field['options'] : null;
             $function = isset($field['function']) ? $field['function'] : null;
             $label = isset($field['label']) ? $field['label'] : ucfirst($key);
+            unset($field['check']);
 
             // default values
             $field['type'] = isset($field['type']) ? $field['type'] : 'text';
@@ -112,8 +305,55 @@ class WalrusForm
 
             if ($field['type'] == 'select') {
 
-                // @TODO: do from YAML & from Function
+                if (!($options xor $function)) {
+                    continue;
+                }
 
+                unset($field['placeholder']);
+
+                // Create label
+                if (!empty($label)) {
+
+                    $Label = new Tag();
+                    $Label->create('label');
+
+                    if (is_string($label)) {
+                        $Label->setAttributes('text', $label);
+                    } elseif (is_array($label)) {
+                        $Label->setAttributes($label);
+                    }
+
+                    array_push($row, $Label);
+                }
+
+                // Create select
+                $Tag = new Tag();
+                $Tag->create('select');
+                $Tag->setAttributes($field);
+
+                // function cal
+                if ($function) {
+                    $cb = explode(':', $function);
+                    $options = WalrusRouter::reroute($cb[0], $cb[1]);
+                }
+
+                foreach ($options as $inputKey => $text) {
+                    // Create option
+                    $Option = new Tag();
+                    $Option->create('option');
+                    $Option->setAttributes(array('value' => $inputKey));
+                    $Option->inject($text);
+                    $Tag->inject($Option);
+                }
+
+                array_push($row, $Tag);
+
+                // Display errors
+                if ($Error = $this->getError($field['name'])) {
+                    array_push($row, $Error);
+                }
+
+                array_push($inputs, $row);
             } elseif ($field['type'] == 'radio' || $field['type'] == 'checkbox') {
 
                 if (!$options) {
@@ -123,11 +363,26 @@ class WalrusForm
                 // remove useless attributes if exists
                 unset($field['placeholder']);
 
+                // Create label
+                if (!empty($label)) {
+
+                    $Label = new Tag();
+                    $Label->create('label');
+
+                    if (is_string($label)) {
+                        $Label->setAttributes('text', $label);
+                    } elseif (is_array($label)) {
+                        $Label->setAttributes($label);
+                    }
+                    array_push($row, $Label);
+                }
+
+                $originalName = $field['name'];
                 $field['name'] .= '[]';
 
                 foreach ($options as $inputKey => $attributes) {
 
-                    $row = array();
+                    $subrow = array();
 
                     if (is_string($attributes)) {
                         $value = $attributes;
@@ -157,7 +412,6 @@ class WalrusForm
                             $attributes['label']['for'] : $attributes['id'];
 
                         $Label->setAttributes($attributes['label']);
-                        array_push($row, $Label);
                     }
                     unset($attributes['label']);
 
@@ -166,10 +420,17 @@ class WalrusForm
                     $Tag->create('input');
                     $Tag->setAttributes(array_merge($field, $attributes));
 
-                    array_push($row, $Tag);
-                    array_push($inputs, $row);
+                    array_push($subrow, $Tag);
+                    array_push($subrow, $Label);
+                    array_push($row, $subrow);
                 }
 
+                // Display errors
+                if ($Error = $this->getError($originalName)) {
+                    array_push($row, $Error);
+                }
+
+                array_push($inputs, $row);
             } else {
 
                 // Create label
@@ -186,11 +447,18 @@ class WalrusForm
                     array_push($row, $Label);
                 }
 
-                // Basic inputs tag
+                // Create input
                 $Tag = new Tag();
                 $Tag->create('input');
                 $Tag->setAttributes($field);
+
                 array_push($row, $Tag);
+
+                // Display errors
+                if ($Error = $this->getError($field['name'])) {
+                    array_push($row, $Error);
+                }
+
                 array_push($inputs, $row);
             }
         }
@@ -203,6 +471,7 @@ class WalrusForm
             'value' => $form['submit']
         ));
         array_push($inputs, array($Submit));
+
 
         // Create the form tag
         $Form = new Tag();
@@ -229,12 +498,32 @@ class WalrusForm
             foreach ($row as $keyInput => $input) {
 
                 if ($wrapper_elem) {
+
                     $Welem = new Tag();
                     $Welem->create($wrapper_elem);
-                    $Welem->inject($input->make());
+
+                    // case for checkbox and radio input
+                    if (is_array($input)) {
+                        foreach ($input as $subelem) {
+                            $Welem->inject($subelem->make());
+                        }
+                    } else {
+                        $Welem->inject($input->make());
+                    }
+
                     $inputs[$keyRow][$keyInput] = $Welem->make();
                 } else {
-                    $inputs[$keyRow][$keyInput] = $input->make();
+                    if (is_array($input)) {
+                        foreach ($input as $subelem) {
+                            if (is_string($inputs[$keyRow][$keyInput])) {
+                                $inputs[$keyRow][$keyInput] .= $subelem->make();
+                            } else {
+                                $inputs[$keyRow][$keyInput] = $subelem->make();
+                            }
+                        }
+                    } else {
+                        $inputs[$keyRow][$keyInput] = $input->make();
+                    }
                 }
             }
 
@@ -256,8 +545,28 @@ class WalrusForm
             $Form->inject($Wrows);
         }
 
-        echo $Form->make();
-        die;
+        return $Form->make();
+    }
+
+    /**
+     * @param $name
+     *
+     * @return bool|Tag
+     */
+    private function getError($name)
+    {
+        if (!isset($this->errors[$name])) {
+            return false;
+        }
+
+        $Error = new Tag();
+        $Error->create('span');
+        $Error->setAttributes(array(
+           'class' => 'error',
+            'text' => $this->errors[$name]
+        ));
+
+        return $Error;
     }
 
     /**
@@ -268,13 +577,13 @@ class WalrusForm
         $form = $this->form;
 
         // default form configuration
-        $form['form'] = isset($form['form'])? $form['form'] : array();
-        $form['form']['method'] = isset($form['form']['method']) ? $form['form']['method'] : 'POST';
-        $form['form']['submit'] = isset($form['form']['submit']) ? $form['form']['submit'] : 'Submit';
-        $form['form']['structure'] = isset($form['form']['structure']) ? $form['form']['structure'] : 'list';
-        $form['form']['action'] = isset($form['form']['action']) ?
-            $form['form']['action'] : $_ENV['W']['route_current'];
+        $form = is_array($form) ? $form : array();
+        $form['method'] = strtoupper(isset($form['method']) ? $form['method'] : 'POST');
+        $form['submit'] = isset($form['submit']) ? $form['submit'] : 'Submit';
+        $form['structure'] = isset($form['structure']) ? $form['structure'] : 'list';
+        $form['action'] = isset($form['action']) ? $form['action'] : $_ENV['W']['route_current'];
 
+        $this->form = $form;
         return true;
     }
 }
