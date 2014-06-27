658f802ac2f1c5a276ecbc2d239325c8cc437bb8
Nicolas Beauvais <nicolas@bvs.email>
2014-06-27 14:49:11 +0200

    Form builder: add textarea exemple, fix min/max verification

--- Walrus/core/WalrusForm.php
+++ Walrus/core/WalrusForm.php
@@ -206,13 +206,13 @@ class WalrusForm
             if (isset($check['odd']) && $check['odd'] == true && $data[$name] % 2 == 0) {// check odd
                 $errors[$name] = WalrusI18n::get('errors', 'messages', 'odd', array('attribute' => $name));
             }
-            if (isset($check['max']) && $check['max'] == true && $data[$name] > $check['max']) {// check max
+            if (isset($check['max']) && $check['max'] == true && strlen($data[$name]) > $check['max']) {// check max
                 $errors[$name] = WalrusI18n::get('errors', 'messages', 'max', array(
                     'attribute' => $name,
                     'count' => $check['max']
                 ));
             }
-            if (isset($check['min']) && $check['min'] == true && $data[$name] < $check['min']) {// check min
+            if (isset($check['min']) && $check['min'] == true && strlen($data[$name]) < $check['min']) {// check min
                 $errors[$name] = WalrusI18n::get('errors', 'messages', 'min', array(
                     'attribute' => $name,
                         'count' => $check['min']
