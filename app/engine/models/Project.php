4a9bda88e05098a80ce1aa20fe77ec15cf2bb233
Anthon95 <anthony.meyer.95@gmail.com>
2014-06-30 15:01:48 +0200

    a cool project model

--- app/engine/models/Project.php
+++ app/engine/models/Project.php
@@ -8,4 +8,66 @@ use Walrus\core\WalrusModel;
 class Project extends WalrusModel
 {
 
+    /**
+     * Create a new project
+     *
+     * @param array $params
+     * @return int|string
+     */
+    public function createProject($params){
+
+        $project = R::dispense("project");
+
+        foreach($params as $key => $value){
+            $project[$key] = $value;
+        }
+        $project['created_at'] = R::isoDateTime();
+        $project['updated_at'] = R::isoDateTime();
+
+        $id = R::store($project);
+        return $id;
+    }
+
+    /**
+     * @param int $id
+     * @param array $params
+     * @return int
+     */
+    public function updateProject($id,$params){
+
+        $project = R::load("project",$id);
+
+        foreach($params as $key => $value){
+            $project[$key] = $value;
+        }
+        $project['updated_at'] = R::isoDateTime();
+
+        R::store($project);
+        return $id;
+
+    }
+
+    /**
+     * Delete an existing project
+     *
+     * @param int $id
+     */
+    public function deleteProject($id){
+
+       $project = R::load("project",$id);
+       R::trash($project);
+    }
+
+    /**
+     * Find an existing project
+     *
+     * @param int $id
+     * @return \RedBean_OODBBean
+     */
+    public function findProject($id){
+
+        $project = R::load("project",$id);
+        return $project;
+    }
+
 }
